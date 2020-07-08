b2680523801f8a7fc6833369589295b474c93499
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-07-07 23:49:25 -0300

    Create initial layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import { Button, Container } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Container, AppBar, Toolbar, IconButton, Typography, Box, LinearProgress, GridList, Fab, makeStyles, GridListTile } from '@material-ui/core';
+
+import MenuIcon from '@material-ui/icons/Menu'
+import AddIcon from '@material-ui/icons/Add'
+
+const useStyles = makeStyles((theme) => ({
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  }
+}));
 
 function App() {
-  return <Container>
-    <Button>ajskdl</Button>
-  </Container>
+  const [loading, setLoading] = useState(false)
+  const [images, setImages] = useState([])
+
+  const classes = useStyles();
+
+  return (
+    <Box>
+      <AppBar position='static'>
+        <Toolbar variant='dense'>
+          <IconButton edge='start' color='inherit' aria-label='menu'>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant='h6' color='inherit'>
+            Photos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {loading && <LinearProgress />}
+      <Container>
+        <GridList
+          cellHeight={160}
+          cols={3}
+          spacing={8}
+        >
+         {images.map((image,index)=> <GridListTile key={index}><img src={image} alt={`${index}`} /></GridListTile>)} 
+        </GridList>
+        <Fab color='primary' aria-label='add' className={classes.fab}>
+          <AddIcon />
+        </Fab>
+      </Container>
+    </Box>
+  )
 }
 
 export default App;
